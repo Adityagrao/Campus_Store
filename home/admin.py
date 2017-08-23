b585447ae73d6ca90899a8e01605a817a53877c6
adityagrao <adityagrao@live.com>
2017-08-23 22:51:16 +0530

    Update
    Signed-off-by: adityagrao <adityagrao@live.com>

--- home/admin.py
+++ home/admin.py
@@ -5,14 +5,16 @@ from .models import School, Book
 
 
 class SchoolAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(School, SchoolAdmin)
+    list_display = ('name', 'phone_no', 'principal_name', 'website', 'code', 'status')
+    list_filter = ('name', 'status')
+    search_fields = ('name', 'company')
 
 
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('School_name', 'class_name', 'bundle', 'particulars', 'total')
+    search_fields = ('School_name', 'class_name')
+    list_filter = ('School_name','class_name','bundle')
 
 
+admin.site.register(School, SchoolAdmin)
 admin.site.register(Book, BookAdmin)
--- home/models.py
+++ home/models.py
@@ -9,19 +9,20 @@ class School(models.Model):
     address = models.CharField(max_length=200)
     phone_no = models.CharField(max_length=10)
     email = models.EmailField(max_length=50)
-    a_email = models.CharField(max_length=50)
+    alternate_email = models.CharField(max_length=50)
     principal_name = models.CharField(max_length=100)
     principal_email = models.EmailField(max_length=50)
     password = models.CharField(max_length=256)
     code = models.CharField(max_length=10, unique=True)
+    status = models.BooleanField(default=True)
 
-class Book (models.Model):
-    school_name = models.ForeignKey(School)
+
+class Book(models.Model):
+    School_name = models.CharField(max_length=50)
     class_name = models.CharField(max_length=50)
     bundle = models.CharField(max_length=20)
     particulars = models.CharField(max_length=50)
-    amount = models.DecimalField(max_digits=7,decimal_places=3,default=0)
-    tax_CGST = models.DecimalField(max_digits=7,decimal_places=3,default=0)
-    tax_SGST = models.DecimalField(max_digits=7,decimal_places=3,default=0)
-    total = models.DecimalField(max_digits=7,decimal_places=3,default=0)
-
+    amount = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    tax_CGST = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    tax_SGST = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    total = models.DecimalField(max_digits=7, decimal_places=2, default=0)
