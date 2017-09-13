445a90a74434cdc0c265bd6542fd29d15c7373c5
adityagrao <adityagrao@live.com>
2017-09-14 00:56:53 +0530

    Update
    Signed-off-by: adityagrao <adityagrao@live.com>

--- Campus_Store/settings/local.py
+++ Campus_Store/settings/local.py
@@ -74,7 +74,10 @@ WSGI_APPLICATION = 'Campus_Store.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': dict(ENGINE='django.db.backends.mysql', NAME='campusstore', USER='root', PASSWORD='')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Password validation
