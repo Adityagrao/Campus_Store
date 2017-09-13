f37b14204ae3ccc8a41ea0bb10c7800e96ed9e1e
adityagrao <adityagrao@live.com>
2017-09-14 01:05:16 +0530

    Update
    Signed-off-by: adityagrao <adityagrao@live.com>

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,4 @@ admin.site.site_header="Campus Store Super Admin"
 admin.site.site_title="Campus Store Super Admin"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^admin/', admin.site.urls),
-
 ]
