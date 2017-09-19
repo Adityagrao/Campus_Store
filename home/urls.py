a7f176e01c790a6ae62bd522ba00e771f58abf1e
adityagrao <adityagrao@live.com>
2017-09-19 18:03:09 +0530

    updated home page links
    Signed-off-by: adityagrao <adityagrao@live.com>

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from . import views
 
-admin.site.site_header="Campus Store Super Admin"
-admin.site.site_title="Campus Store Super Admin"
+admin.site.site_header = "Campus Store Super Admin"
+admin.site.site_title = "Campus Store Super Admin"
+
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index')
 ]
